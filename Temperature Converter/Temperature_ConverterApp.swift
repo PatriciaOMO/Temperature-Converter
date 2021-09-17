4e49d7f3ba1420205aad158667fad36b486875e1
Tish <tish302@gmail.com>
2021-09-17 13:19:41 -0600

    Initial Commit

---
+++ Temperature Converter/Temperature_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Temperature_ConverterApp.swift
+//  Temperature Converter
+//
+//  Created by Patricia Obregon on 9/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Temperature_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
